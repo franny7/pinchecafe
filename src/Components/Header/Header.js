f74882dfe1c0d7b0a374c07377c6a9b6e85ddcae
Luis Inzunza <luisinzu08@gmail.com>
2021-03-10 13:55:06 -0800

    update header component,remove button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button';
 import './Header.css'
 
 const Header = () => {
     return (
-        <Jumbotron className="Jumbo">
-            <h1>Welcome to ¡Pinchè Cafè!</h1>
+        <div className="Jumbotron">
+            <Jumbotron className="Jumbo">
+            <h1>Welcome to <br />¡Pinchè Cafè!</h1>
                 <p>
                     This is a simple hero unit, a simple jumbotron-style component for calling
                     extra attention to featured content or information.
                 </p>
-                <p>
-                    <Button variant="primary">Learn more</Button>
-                </p>
         </Jumbotron>
+        </div>
+        
     )
 }
 
