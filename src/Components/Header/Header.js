382f5c56e759a611395fc9f6c5fd1109e5fe3d0e
Francisco Martinez <panchetz7@gmail.com>
2021-03-13 13:59:29 -0800

    add coffee images

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import './Header.css'
+import './Header.css';
 
 const Header = () => {
-    return (
-        <div>
-            <Jumbotron>
-                <h1>Welcome to <br />¡Pinchè Cafè!</h1>
-                    <p>
-                        This is a simple hero unit, a simple jumbotron-style component for calling
-                        extra attention to featured content or information.
-                    </p>
-            </Jumbotron>
-        </div>
-        
-    )
-}
+  return (
+    <div>
+      <Jumbotron>
+        <h1>
+          Welcome to <br />
+          ¡Pinchè Cafè!
+        </h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+      </Jumbotron>
+    </div>
+  );
+};
 
 export default Header;
