c1c0e1beb7b8b3a8b46c6c817f058f50ed209233
Luis Inzunza <luisinzu08@gmail.com>
2021-03-12 22:31:05 -0800

    some changes to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,14 +4,14 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className="Jumbotron">
-            <Jumbotron className="Jumbo">
-            <h1>Welcome to <br />¡Pinchè Cafè!</h1>
-                <p>
-                    This is a simple hero unit, a simple jumbotron-style component for calling
-                    extra attention to featured content or information.
-                </p>
-        </Jumbotron>
+        <div>
+            <Jumbotron>
+                <h1>Welcome to <br />¡Pinchè Cafè!</h1>
+                    <p>
+                        This is a simple hero unit, a simple jumbotron-style component for calling
+                        extra attention to featured content or information.
+                    </p>
+            </Jumbotron>
         </div>
         
     )
