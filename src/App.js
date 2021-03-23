585e9f2348811be39123e9dcd03106dd47f2a9b6
Luis Inzunza <luisinzu08@gmail.com>
2021-03-22 23:00:41 -0700

    add cards component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header'
 import Navbar from './Components/Nav/Nav';
+import Cards from './Components/Cards/Cards'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Cards />
     </div>
   );
 }
