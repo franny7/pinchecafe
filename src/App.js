c0fcdd114b42eb6e18fee7417a90abd212c323fe
Francisco Martinez <panchetz7@gmail.com>
2021-04-05 22:26:55 -0700

    update popular drinks nav link

--- src/App.js
+++ src/App.js
@@ -3,26 +3,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Nav/Nav';
-HEAD
-import Cards from './Components/Cards/Cards'
+import Cards from './Components/Cards/Cards';
 import Footer from './Components/Footer/Footer';
-footer
 import AboutUs from './Components/AboutUs/AboutUs';
-4534e3ffaeaddec0282c0c2086dd7442c2a0d485
-master
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <Header />
-footer
       <AboutUs />
-HEAD
       <Cards />
-master
       <Footer />
-4534e3ffaeaddec0282c0c2086dd7442c2a0d485
     </div>
   );
 }
