b31f4a41108d18e950a7c7c7dc87a0ecd920c8db
Francisco Martinez <panchetz7@gmail.com>
2021-04-10 15:15:55 -0700

    cards styling & expand footer floater

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -4,15 +4,21 @@ import Card from 'react-bootstrap/Card';
 
 const Cards = () => {
   return (
-    <div id='popular-drinks'>
+    <div id='popular-drinks' className='border-bottom border-light border-1'>
       <CardGroup>
-        <Card>
+        <Card
+          style={{
+            backgroundColor: '#212529',
+            color: 'white',
+            borderColor: '#baa375',
+          }}
+        >
           <Card.Img
             variant='top'
             src='https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG'
           />
           <Card.Body>
-            <Card.Title>Drink #1</Card.Title>
+            <Card.Title className='fs-3'>Iced Latte</Card.Title>
             <Card.Text>
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This content is a little bit
@@ -20,32 +26,44 @@ const Cards = () => {
             </Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className='text-muted'>Last updated 3 mins ago</small>
+            <small style={{ color: '#baa529' }}>Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card>
+        <Card
+          style={{
+            backgroundColor: '#212529',
+            color: 'white',
+            borderColor: '#baa375',
+          }}
+        >
           <Card.Img
             variant='top'
             src='https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG'
           />
           <Card.Body>
-            <Card.Title>Drink #2</Card.Title>
+            <Card.Title className='fs-3'>Frappuccino</Card.Title>
             <Card.Text>
               This card has supporting text below as a natural lead-in to
               additional content.{' '}
             </Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className='text-muted'>Last updated 3 mins ago</small>
+            <small style={{ color: '#baa529' }}>Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card>
+        <Card
+          style={{
+            backgroundColor: '#212529',
+            color: 'white',
+            borderColor: '#baa375',
+          }}
+        >
           <Card.Img
             variant='top'
             src='https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG'
           />
           <Card.Body>
-            <Card.Title>Drink #3</Card.Title>
+            <Card.Title className='fs-3'>Cold Brew</Card.Title>
             <Card.Text>
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This card has even longer content
@@ -53,7 +71,7 @@ const Cards = () => {
             </Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className='text-muted'>Last updated 3 mins ago</small>
+            <small style={{ color: '#baa529' }}>Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
       </CardGroup>
